dee225aee230faba71b6a305cbeaa8be5491699a
ksenia <kseniyanovikova2008@gmail.com>
2026-03-01 14:12:26 +0300

    Remove hardcoded ports, add host (lab01)

--- lab01/cmd/app/main.go
+++ lab01/cmd/app/main.go
@@ -10,13 +10,14 @@ import (
 
 func main() {
 	// read os.Getenv
+	host := os.Getenv("APP_HOST")
 	port := os.Getenv("APP_PORT")
 
 	// handlers
 	http.Handle("/health", check_health.New())
 
-	log.Println("Listening http at port: ", port)
-	err := http.ListenAndServe(fmt.Sprintf(":%s", port), nil)
+	log.Printf("Listening http at addr: %s:%s", host, port)
+	err := http.ListenAndServe(fmt.Sprintf("%s:%s", host, port), nil)
 	if err != nil {
 		log.Fatalln("Cannot listen http", err)
 	}
