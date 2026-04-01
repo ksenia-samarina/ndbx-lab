37f62a9593131d9243114e5b53445f093b9f2fe1
ksenia <kseniyanovikova2008@gmail.com>
2026-04-01 16:49:00 +0300

    Fix return err lab03

--- service/internal/repository/redis/get_internal_user_id.go
+++ service/internal/repository/redis/get_internal_user_id.go
@@ -2,12 +2,18 @@ package redis
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"samarina/ndbx/internal/domains/types"
+
+	redisdb "github.com/redis/go-redis/v9"
 )
 
 func (s *Storage) GetInternalUserID(ctx context.Context, sid *types.Sid) (string, error) {
 	userID, err := s.client.HGet(ctx, sid.SidString, "user_id").Result()
+	if errors.Is(err, redisdb.Nil) {
+		return "", nil
+	}
 	if err != nil {
 		return "", fmt.Errorf("error get user_id from user session: %w", err)
 	}
